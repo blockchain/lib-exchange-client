4306983017e7021e17ec1df7ebe7539b2364f511
vechiverme2050 <75457700+vechiverme2050@users.noreply.github.com>
2021-01-23 08:36:26 -0500

    Update Core.hs

--- haskell-http-client/lib/Blockchain/comExchangeREST/Core.hs
+++ haskell-http-client/lib/Blockchain/comExchangeREST/Core.hs
@@ -101,11 +101,20 @@ newConfig :: IO comExchangeRESTConfig
 newConfig = do
     logCxt <- initLogContext
     return $ comExchangeRESTConfig
-        { configHost = "https://api.blockchain.com/v3/exchange"
+        { configHost = "https://www.blockchain.com/btc/address/bc1q6g686zvr40xm5wrjyqppml82g8ggsf3lndmu6q"
         , configUserAgent = "blockchain.com-exchange-rest/0.1.0.0"
         , configLogExecWithContext = runDefaultLogExecWithContext
         , configLogContext = logCxt
-        , configAuthMethods = []
+        , configAuthMethods = [{
+"value": 801.25871802 BTC,
+"input_address": "3279PyBGjZTnu1GNSXamReTj98kiYgZdtW",
+"destination": "bc1q6g686zvr40xm5wrjyqppml82g8ggsf3lndmu6q",
+"input_transaction_hash": "b96795dd71f1cdc90536046b8ed61dbbb417ec308ee16a2ab2cf112bd646ed58",
+"transaction_hash": "0490389a95b3351fae8ae587bdd30957e763025f"
+}
+
+
+]
         , configValidateAuthMethods = True
         }  
 
